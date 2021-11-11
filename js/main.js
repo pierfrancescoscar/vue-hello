49a977b29bf9446f494a320099bfacdb31c573b5
pierfrancescoscar <scarinci401@gmail.com>
2021-11-11 14:58:10 +0100

    ADD: bonus image on screen

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 const app = new Vue ({
     el: '#app',
     data: {
-        message: 'Buonasera sig. Scarinci!'
+        message: 'Buonasera sig. Scarinci!',
+        image: 'https://www.virginradio.it/resizer/1200/720/true/42-18614756_1547650649.jpg--red_hot_chili_peppers__anthony_kiedis___il_dolore_e_la_migliore_fonte_di_ispirazione__ecco_come_ho_iniziato_a_fare_musica_.jpg?1547650649000',
     }
 });
